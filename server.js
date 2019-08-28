03c6f41b5c0fbb99219a151a04005b35c462292f
yaojm <yaojm@guahao.com>
2019-08-28 20:16:11 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.listen(8000,()=>{
   console.log('server is running');
 });
+
+app.use(bodyParser.json());
 
 const mockUserData = [
   {
@@ -22,4 +25,24 @@ app.get('/users', (req, res) => {
 });
 app.get('/users/:id', (req, res) => {
   console.log(req.params.id);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(req.body, username, password);
+  const mockUsername = 'billTheKid';
+  const mockPasswork = 'superSecret';
+  if (username === mockUsername && password === mockPasswork) {
+    res.json({
+      success: true,
+      message: 'password and username match!',
+      token: 'encrypted token goes here',
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'password and username do not match',
+    });
+  }
 });
